acba97bc97f78f045982764f76c2408aedbf32e2
Eranda Bandaranaike <ergroove@live.com>
2025-04-01 11:14:12 +0530

    Basic API route has been initiated

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\TaskController;
+
+Route::prefix('tasks')->group(function () {
+    Route::get('/', [TaskController::class, 'index']);
+    Route::get('/{id}', [TaskController::class, 'show']);
+    Route::post('/', [TaskController::class, 'store']);
+    Route::put('/{id}', [TaskController::class, 'update']);
+    Route::delete('/{id}', [TaskController::class, 'destroy']);
+});
